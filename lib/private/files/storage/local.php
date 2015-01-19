a435d07b397e64c82da43cb2bb18a6621d0d26fe
Robin Appelman <icewind@owncloud.com>
2015-01-19 16:25:28 +0100

    Make getSourcePath accessible for storage wrappers

--- lib/private/files/storage/local.php
+++ lib/private/files/storage/local.php
@@ -308,7 +308,7 @@ if (\OC_Util::runningOnWindows()) {
 		 * @param string $path
 		 * @return string
 		 */
-		protected function getSourcePath($path) {
+		public function getSourcePath($path) {
 			$fullPath = $this->datadir . $path;
 			return $fullPath;
 		}
